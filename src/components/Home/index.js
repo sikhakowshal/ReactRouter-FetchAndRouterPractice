9622be2991d61deb4561408d214f68d987d54688
Sikha Kowshal <koushalsikha4@gmail.com>
2023-06-21 12:07:44 +0000

    final-code

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import UserInfo from '../UserInfo'
+import BlogList from '../BlogList'
+
+import './index.css'
+
+const Home = () => (
+  <div className="home-container" data-testid="loader">
+    <UserInfo />
+    <BlogList />
+  </div>
+)
+
+export default Home
